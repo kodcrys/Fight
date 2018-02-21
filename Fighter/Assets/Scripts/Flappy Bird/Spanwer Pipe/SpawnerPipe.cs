b72971154fd0482dd540ad70db94a2913c8f2064
Hiep <luvluv2410@gmail.com>
2018-02-21 11:42:50 +0700

    Update flappy thumb by Hiep

---
+++ Fighter/Assets/Scripts/Flappy Bird/Spanwer Pipe/SpawnerPipe.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnerPipe : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject pipeHolder;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (Spawner ());
+	}
+	
+	IEnumerator Spawner(){
+		yield return new WaitForSeconds (1.4f);
+		Vector3 temp = pipeHolder.transform.position;
+		temp.y = Random.Range (-1.5f, 1.5f);
+		Instantiate (pipeHolder, temp, pipeHolder.transform.rotation);
+		//Instantiate (pipeHolder, temp, Quaternion.identity);
+		StartCoroutine (Spawner ());
+	}
+}
