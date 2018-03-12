92a87ffd149c353482a4b7e3158626de9c277eba
KogNo1 <boy231193@gmail.com>
2018-03-12 09:48:55 +0700

    Delete FingerControler by Tung

---
+++ Fighter/Assets/Scripts/Game/AnimationFadeManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class AnimationFadeManager : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+[Serializable]
+public class FadeAnimOption{
+	public List<GameObject> fadeAnimOption = new List<GameObject>();
+}
