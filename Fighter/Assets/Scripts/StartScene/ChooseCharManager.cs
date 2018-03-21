3109a750d2a0e7c4e44e35f045faec63839775ee
ManhNguyen <manh.nguyen.kod@gmail.com>
2018-03-21 14:32:11 +0700

    asdad

---
+++ Fighter/Assets/Scripts/StartScene/ChooseCharManager.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ChooseCharManager : MonoBehaviour {
+
+	public static ChooseCharManager instance;
+
+	[Header("Button change type choose")]
+	[SerializeField]
+	UnityEngine.UI.Button[] btnsChangeType;
+	[SerializeField]
+	Transform [] PosBtnsChangeType;
+
+	[SerializeField]
+	GameObject typeChooseChar;
+	[SerializeField]
+	GameObject typeChooseEquipment;
+
+	[Header("Data")]
+	[SerializeField]
+	CointainData[] dataChars;
+	[SerializeField]
+	CointainData[] dataItems;
+
+	[Header("Choose Action")]
+	public GameObject chooseSymbol;
+
+	[Header("Mask")]
+	[SerializeField]
+	Transform maskFrameChooseChar;
+	[SerializeField]
+	Transform maskFrameChooseEquip;
+
+	[Header("PLayer1")]
+	[SerializeField]
+	UnityEngine.UI.Image hatSymbol;
+	[SerializeField]
+	UnityEngine.UI.Image hatMainR;
+	[SerializeField]
+	UnityEngine.UI.Image amorMainR;
+	[SerializeField]
+	UnityEngine.UI.Image weaponMainR;
+
+	[Header("PLayer2 or AI")]
+	[SerializeField]
+	UnityEngine.UI.Image hatSymbol2;
+	[SerializeField]
+	UnityEngine.UI.Image hatMainL;
+	[SerializeField]
+	UnityEngine.UI.Image amorMainL;
+	[SerializeField]
+	UnityEngine.UI.Image weaponMainL;
+
+	bool isTurnPlayer1;
+
+	GameObject objFollow;
+
+	[HideInInspector]
+	public bool isPlayAI;
+
+	void Awake() {
+		if (instance == null)
+			instance = this;
+	}
+
+	// Use this for initialization
+	void Start () {
+		isTurnPlayer1 = true;
+		chooseSymbol.SetActive (false);
+	}
+
+	void Update() {
+		if(objFollow != null)
+			chooseSymbol.transform.position = objFollow.transform.position;
+	}
+
+	bool isShowTypeChar = true;
+	public void ChangeTypeChar() {
+		isShowTypeChar = !isShowTypeChar;
+		chooseSymbol.SetActive (false);
+		if (isShowTypeChar) {
+			btnsChangeType [0].transform.SetParent (PosBtnsChangeType [1].transform);
+			btnsChangeType [1].transform.SetParent (PosBtnsChangeType [0].transform);
+			typeChooseChar.SetActive (true);
+			typeChooseEquipment.SetActive (false);
+		} else {
+			btnsChangeType [1].transform.SetParent (PosBtnsChangeType [1].transform);
+			btnsChangeType [0].transform.SetParent (PosBtnsChangeType [0].transform);
+			typeChooseChar.SetActive (false);
+			typeChooseEquipment.SetActive (true);
+		}
+	}
+
+	void EnDisableLibraryCell() {
+		for (int i = 0; i < dataChars.Length; i++) {
+			if (dataChars [i].dataChar.isOwned == false)
+				dataChars [i].GetComponent<UnityEngine.UI.Button> ().interactable = false;
+			else
+				dataChars [i].GetComponent<UnityEngine.UI.Button> ().interactable = true;
+		}
+
+		for (int i = 0; i < dataItems.Length; i++) {
+			if (dataItems [i].dataItem.isOwned == false)
+				dataItems [i].GetComponent<UnityEngine.UI.Button> ().interactable = false;
+			else
+				dataItems [i].GetComponent<UnityEngine.UI.Button> ().interactable = true;
+		}
+	}
+
+	public void ChooseChar() {
+		GameObject gob = EventSystem.current.currentSelectedGameObject;
+		//chooseSymbol.transform.position = gob.transform.position;
+		objFollow = gob;
+		if (isShowTypeChar)
+			chooseSymbol.transform.SetParent (maskFrameChooseChar);
+		else
+			chooseSymbol.transform.SetParent (maskFrameChooseEquip);
+		//chooseSymbol.transform.SetParent (gob.transform);
+		chooseSymbol.SetActive (true);
+		CointainData ctData = gob.GetComponent<CointainData> ();
+		if (ctData.dataChar != null) {
+			hatSymbol2.gameObject.SetActive (false);
+			hatMainL.sprite = ctData.dataChar.equipmentOfChar;
+			amorMainL.gameObject.SetActive (false);
+			weaponMainL.gameObject.SetActive (false);
+		}
+		//	Library.Instance.ReadInfoCharacter (ctData.dataChar);
+		//if (ctData.dataItem != null)
+		//	Library.Instance.ReadInfoEquipment (ctData.dataItem);
+	}
+}
